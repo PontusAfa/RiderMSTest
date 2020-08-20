6af872ff01f3ebd839eb53937039e1bf81303a5e
Pontus Jonsson <Pontus.Jonsson@afaforsakring.se>
2020-08-20 15:20:47 +0200

    test2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO.Compression;
+using testTest;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Test();
+            Console.WriteLine("Hello");
+            var count = t1.Calc();
+        }
+    }
+}
